09809589f570586fa9a5747664f900f6f940e1df
Nishanth <52668520+nishanthmessi@users.noreply.github.com>
2022-08-13 14:46:14 +0530

    Add files via upload

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,4 +1,4 @@
-import {FaGithub} from 'react-icons/fa'
+import {FaGithubAlt} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 
@@ -7,7 +7,7 @@ const Navbar = ({ title }) => {
     <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
-          <FaGithub className='inline pr-2 text-3xl'/>
+          <FaGithubAlt className='inline pr-2 text-3xl'/>
           <Link to='/' className='text-lg font-bold align-middle'>
             {title}
           </Link>
